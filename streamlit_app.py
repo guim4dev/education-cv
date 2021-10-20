82a88e508f602ba3528121ed73b2d47f71eda38e
Bruno Dantas de Paiva <bruno.dantas@poli.ufrj.br>
2021-10-20 01:45:39 -0300

    trying to fix st.success

--- streamlit_app.py
+++ streamlit_app.py
@@ -78,7 +78,8 @@ if google_drive != '':
     drive_block.empty()
     with st.spinner('Aguarde enquanto processamos sua aula.'):
         time.sleep(5)
-    st.success('Pronto! ID da Aula processada: 182916f6-756d-40d6-95fc-3283ba5efdf8')
+        st.success('Pronto! ID da Aula processada: 182916f6-756d-40d6-95fc-3283ba5efdf8')
+    google_drive = ''
 
 if is_authenticated(password):
     id_block.empty()
